ed88b9fca1e84f08f85ae78eaa1f804ce1f94525
Nestor Mansipe <electrohelps2019@gmail.com>
2022-02-06 21:14:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//import modulo from "./calculadora"
+//import { resta } from "./calculadora";
+//import { suma } from "./calculadora"
+
+
+// const suma = (a + b) => {
+ //   console.log(a,b)
+  //  return a + b
+//}
+
+
+//const suma = (a, b) =>  a + b
+
+//const suma = (a, b) => {
+//    console.log(a, b)
+//    return a + b
+//}
+
+//const resultado = suma(1, 2)
+
+//console.log(resultado)
+
+// function sinPromesa() {
+//     return Promise.resolve(1)
+//     .then(x => x + 1)
+//     .then(x => x + 1)
+//     .then(console.log)
+//     sinPromesa()
+//}
+// async function conPromesa(){
+//     const resultado = await Promise.resolve(1)
+//     console.log(resultado + 1 + 1 )
+
+// }
+// conPromesa()
+
